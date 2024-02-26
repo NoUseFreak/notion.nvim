0164623be1495793d136e9c46559459534c0211a
Dries De Peuter <dries@stenic.io>
2024-02-26 01:44:34 +0100

    fix: Keep telescope open after opening url

--- lua/notion/actions.lua
+++ lua/notion/actions.lua
@@ -10,6 +10,7 @@ end
 
 local M = {}
 
+-- Insert issue number in buffer
 M.issue_insert = function(prompt_bufnr)
   local selection = close_telescope_prompt(prompt_bufnr)
   if vim.api.nvim_buf_get_option(vim.api.nvim_get_current_buf(), 'modifiable') then
@@ -17,9 +18,9 @@ M.issue_insert = function(prompt_bufnr)
   end
 end
 
-M.webview_issue = function(prompt_bufnr)
-  -- Open issue in webview
-  local selected = close_telescope_prompt(prompt_bufnr)
+-- Open issue in webview
+M.webview_issue = function(_)
+  local selected = action_state.get_selected_entry()
   if vim.fn.has('mac') == 1 then
     vim.fn.system('open ' .. selected._url)
   elseif vim.fn.has('unix') == 1 then
